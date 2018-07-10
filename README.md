64194d8d7071462805c1a7bb89d448f63f0aa954
DebarghaG <royal.debargha@gmail.com>
2018-07-10 09:33:00 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Ashokan_SelfDrivingCar
+
+Over the next month , I will attempt to write the code to run a self-driving car around campus, completely autonomously.
---
+++ RaspberryPi_Stream/SingleCamera_StreamClient.py
@@ -0,0 +1,35 @@
+#Single source Python stream from Raspberry Pi camera
+
+import io
+import socket
+import struct
+import time
+import picamera
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('#Enter the preffered IP connect address', 8000))
+
+connection = client_socket.makefile('wb'))
+
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution(320,240) #Trying not to stream higher definition
+        camera.framerate = 10
+        time.sleep(2)
+        start = time.time()
+        stream= io.BytesIO()
+
+        #Send the video stream in a jpeg format
+        for foo in camera.capture_continuous(stream, 'jpeg', use_video_port = True):
+            connection.write(struct.pack('L', stream.tell()))
+            connection.flush
+            stream.seek(0)
+            connection.write(stream.read())
+            if time.time() - start > 600:
+                break
+            stream.seek(0)
+            stream.truncate()
+    connection.write(struct.pack('L', 0))
+finally:
+    connection.close()
+    client_socket.close()
