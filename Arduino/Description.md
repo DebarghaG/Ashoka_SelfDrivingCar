3d2a20a8a9c45f58fb58e5c75f073e17471fdc20
DebarghaG <royal.debargha@gmail.com>
2018-07-16 01:47:14 +0530

    Modified the actuation controls on the arduino to control the self driving car

--- Arduino/Description.md
+++ Arduino/Description.md
@@ -6,4 +6,20 @@ Stepper motors are used everywhere in actuations because they help with accurate
 
 This will help accurately control the degree at which the entire steering wheel is rotated.
 
-It's important to know that this is not a long term solution but a way to create somewhat of a working hack that can be used with a basic car. 
+It's important to know that this is not a long term solution but a way to create somewhat of a working hack that can be used with a basic car.
+
+## The same arduino controlling the accelerator pedal
+
+Similarly a stepper motor is used for accurate actuation of the accelerator pedal.
+
+## The same arduino is used to control the brake pedal.
+
+Same concept used for the brake pedal.
+
+## Comm with the USB serial port 
+
+This is a jugaad. To minimize the number of ports and the controlling motors, the value being passed to the arduino actually are the entire steering values.
+
+The first two values being passed to the arduino contain the degree to which the steering angle is shifted. The next two values contain the accelerator value, and the last two contain the values at which the brake pedal is to be pushed.
+
+Now the rate at which the communication occurs is very important. This feature hasn't still been implemented. Basically the communication should happen as many times as a second as possible to best control the car.
--- Arduino/StepperControl.ino
+++ Arduino/StepperControl.ino
@@ -2,17 +2,28 @@
 
 #include <Stepper.h> // Include the header file
 
-// Change the number of steps to the final count of the motor being used.
-#define STEPS 32
+// Change the number of STEPS_STEER to the final count of the motor being used.
+#define STEPS_STEER 32
+#define STEPS_ACC 32
+#define STEPS_BRAKE 32
 
 // Creating an instance of the stepper class.
-Stepper stepper(STEPS, 8, 10, 9, 11);
+Stepper stepper_steer(STEPS_STEER, 10, 9, 11);
+// Change the port numbers for the stepper for the accelerator
+Stepper stepper_acc(STEPS_ACC, 6 , 7 , 8 );
+// Change the port numbers for the stepper for the brake
+Stepper stepper_brake(STEPS_BRAKE, 3,4,5);
 
 int val = 0;
+int degree = 0;
+int speed = 0;
+int brake = 0;
 
 void setup() {
   Serial.begin(9600);
-  stepper.setSpeed(200);
+  stepper_steer.setSpeed(200);
+  stepper_acc.setSpeed(200);
+  stepper_brake.setSpeed(200);
 }
 
 void loop() {
@@ -20,9 +31,35 @@ void loop() {
   if (Serial.available()>0)
   {
     val = Serial.parseInt();
+    // This loop now deconstructs val to find the individual values of steer, speed and brake.
+    for(int i=0; i<6: i++)
+    {
+      int rem = val% 10; // This is extracting the last digit value.
+      if(i<2)
+      {
+        brake = brake+(rem*10)
+      }
+      else if( i<4 )
+      {
+        speed = speed+(rem*10)
+      }
+      else
+      {
+        steer = steer+(rem*10)
+      }
+
+      val = val / 10; //Removing the last digit.
+    }
     // Can the computer passes this value to the arduino for the steering input.
-    stepper.step(val);
-    
+    stepper_steer.step(val);
+    stepper_acc.step(speed);
+    stepper_brake.step(brake);
+
+    //Flush the variables :
+    steer = 0;
+    speed = 0;
+    brake = 0; 
+
     Serial.println(val); //for debugging
   }
 
